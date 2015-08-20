ddf4c72eb6ff905a66a259a298b636a165b9b42a
Kevin Coxe <kevcoxe@gmail.com>
2015-08-19 21:37:53 -0400

    new page

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rkweddingApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rkweddingApp
+ */
+angular.module('rkweddingApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rkweddingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rkweddingApp
+ */
+angular.module('rkweddingApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
